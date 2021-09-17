582914c50a24f7e7936d56cdf2721fd1b642b918
Shaurya <72348147+shauryagoyall@users.noreply.github.com>
2021-09-17 17:26:31 +0530

    Update approx gft.py

--- approx gft.py
+++ approx gft.py
@@ -2,11 +2,12 @@
 import numpy as np
 import math
 
-def get_sparse(laplacian, dim): 
-   laplacian = np.tril(laplacian) #convert laplacian to lower triangle to apply givens
-   np.fill_diagonal(laplacian, 0) #convert all diagonal terms to 0 as we want row index > column index for givens
-   laplacian = np.absolute(laplacian) #convert all terms in matrix to their absolute values
-   loc = np.where(laplacian == np.max(laplacian)) #find index of the maximum absolute value in matrix
+def get_sparse(laplacian, dim):
+   
+   lowertri = np.tril(laplacian) #convert laplacian to lower triangle to apply givens
+   np.fill_diagonal(lowertri, 0) #convert all diagonal terms to 0 as we want row index > column index for givens
+   lowertri = np.absolute(lowertri) #convert all terms in matrix to their absolute values
+   loc = np.where(lowertri == np.max(lowertri)) #find index of the maximum absolute value in matrix
    p, q = int(loc[0]) , int(loc[1]) # assign it to p and q
    numer = (laplacian[q][q] - laplacian[p][p])  #as per figure 3 ie the optimization subproblem
    denom = 2*laplacian[p][q]
@@ -19,25 +20,38 @@ def get_sparse(laplacian, dim):
    S[q][p] = (-1)*S[p][q]
    return S 
 
+#np.random.seed(0)
 
-
-#import time #to measure time taken
+import time #to measure time taken
 # laplacian = np.array([[1,2,3, 4 , 6],[2,9,4, 23, 9],[0,-20,-18, 15, 1], [123,734, 63, 1, 7],[234, 85, 34, 8, 0]], float)
-laplacian = np.random.rand(5,5) #makes a random 5 x 5 matrix
+laplacian = np.random.rand(100,100) #makes a random 5 x 5 matrix
 n = np.shape(laplacian)[0]    #find dimension of laplacian 
 order  = 2     #for J
 J = order*n*math.log(n) #taking J to be of order n log n
 print("\nJ is currently", J)
 # J = n^(a) ##   a<2  we can also take J to be of this form
 L = laplacian
-#a = time.time()  #to measure time taken
+a = time.time()  #to measure time taken
 sparse_list = [] 
 for j in range(math.floor(J)): #we take the integer value of J ie. if J = 4.67 then floor(J) will be 4   range(n) starts from 0 to n-1 so a total of n iterations 
     S = get_sparse(L, n)
     S1 = S.transpose()
     L = S1@L@S # the @ sign is for matrix multiplication
     sparse_list.append(S)
-#b = time.time() #to measure time taken
-#print("\n\nTimes taken is", b - a) #to measure time taken
-L = np.diag(np.diag(L)) #take only diagonal terms of L
-print("\n\n",L)
+
+diagonal_elem = np.diag(L) #take only diagonal terms of L in a single 1d array
+#print(diagonal_elem)
+idx = diagonal_elem.argsort()[::] 
+diagonal_elem = diagonal_elem[idx]
+#print(diagonal_elem)
+L = np.diag(diagonal_elem) 
+# print("\n\n",L)
+sparse_list.pop()
+#print("\n\n",S)
+S=S[:,idx]
+#print("\n\n",S)
+sparse_list.append(S)
+
+
+b = time.time() #to measure time taken
+print("\n\nTimes taken is", b - a) #to measure time taken
